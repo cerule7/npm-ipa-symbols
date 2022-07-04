d9af9a807ab262f2442616d0a7ec82c075ffb9f3
Charles Rule <cruleis@gmail.com>
2022-07-04 17:13:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-export * as IPA_SYMBOLS from './ipa_symbols';
-export * as IPA_UTILS from './utils';
+import * as IPA_SYMBOLS from './ipa_symbols';
+import * as IPA_UTILS from './utils';
+
+export default { IPA_SYMBOLS, IPA_UTILS };
